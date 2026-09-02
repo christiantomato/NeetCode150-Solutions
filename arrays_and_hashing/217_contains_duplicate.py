751795709f4849a0cb6a9d40b2626bd374f3740b
christiantomato <christianntamayo@gmail.com>
2026-09-01 20:14:48 -0400

    submission 1

--- arrays_and_hashing/217_contains_duplicate.py
+++ arrays_and_hashing/217_contains_duplicate.py
@@ -1,3 +1,20 @@
+"""
+217. Contains Duplicate
+
+Approach:
+Loop through array once, keep track of encountered numbers 
+and return false if duplicate is found.
+
+Time Complexity: O(n)
+Space Complexity: O(n)
+"""
+
 class Solution:
     def containsDuplicate(self, nums: list[int]) -> bool:
-    
+        num_set = set()
+
+        for num in nums:
+            if num not in num_set:
+                num_set.add(num)
+            else: return True
+        return False 
