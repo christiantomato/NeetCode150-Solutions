fb855a5d30e364955269be7f4b5aed130cb3aa59
christiantomato <christianntamayo@gmail.com>
2026-09-02 08:04:20 -0400

    submission 1

---
+++ arrays_and_hashing/242_valid_anagram.py
@@ -0,0 +1,19 @@
+"""
+242. Valid Anagram
+09/02/26
+
+Approach:
+Keep track of frequency using a hash dictionary where the key is the letter, 
+and the data is the frequency. Serendipitously, Python has a library for this.
+
+Time Complexity: O(N)
+Space Complexity: O(1)
+"""
+
+from collections import Counter
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        #use the built in frequency map
+        return Counter(s) == Counter(t)
+        
