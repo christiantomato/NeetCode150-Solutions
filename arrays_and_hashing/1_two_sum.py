a779f1f922f1551aa9c55b494e04b392e5edfced
christiantomato <christianntamayo@gmail.com>
2026-09-04 17:53:53 -0400

    submission 2

--- arrays_and_hashing/1_two_sum.py
+++ arrays_and_hashing/1_two_sum.py
@@ -1,22 +1,27 @@
 """
 1. Two Sum
-09/03/26
+09/04/26
 
 Approach:
-Brute force by checking sums. Ignore redundant sums and compute
-only the following. 
+Keep track of differences in a hash dictionary which retains the indice, 
+check if following numbers match a difference already computed.
 
-Time Complexity: O(n^2)
-Space Complexity: O(1)
+Time Complexity: O(n)
+Space Complexity: O(n)
 """
 
 class Solution:
     def twoSum(self, nums: list[int], target: int) -> list[int]:
+        #create a hash dictionary, keys: difference with target, data: indices
+        differences = {}
 
         #loop through list
         for i in range(len(nums)):
-            #test sums with only the following numbers 
-            for j in range(i + 1, len(nums)):
-                    if nums[i] + nums[j] == target: return [i, j]
-        
-        
+            #check if current num is a computed difference
+            if nums[i] in differences:
+                #then nums[i] = target - nums[j], so retrieve index j which is stored
+                return [differences[nums[i]], i]
+            else:
+                #otherwise add the difference to hash dictionary
+                difference = target - nums[i]
+                differences[difference] = i
